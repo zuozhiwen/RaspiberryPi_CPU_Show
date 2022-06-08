8b7d7ab9f7f1e4c7143980f5d30497f3c4008f41
zuozhiwen <zzwqqmail@qq.com>
2022-06-08 13:01:15 +0800

    support other info via rabbitmq

--- pcd8544.py
+++ pcd8544.py
@@ -28,35 +28,46 @@ LED = 7
 ON = 0
 OFF = 1
 
+display_mode = 'pi'
 
 def flash_screen(round):
     for i in range(round * 2):
         GPIO.output(LED, i % 2)
         time.sleep(0.2)
 
+def human_readable_size(size):
+    unit = ['B', 'KB', 'MB', 'GB']
+    unit_index = int(math.log(size, 1024))
+    val = size / math.pow(1024, unit_index)
 
-def display_on_screen(uptime, loads, mem_info):
+    final_str = '{0:.1f}'.format(val)[0:4].rstrip('.') + unit[unit_index]
+    return final_str
+
+def display_on_screen(uptime, loads, mem_info, host_name :str = None):
+    if not host_name:
+        host_name = "Raspberry Pi 3"
+    
     # title
     lib.LCDclear()
-    lib.LCDdrawstring(0, 1, b"Raspberry Pi 3")
+    lib.LCDdrawstring(0, 1, host_name.encode())
     lib.LCDdrawline(0, 10, 83, 10, BLACK)
-    
+
     # line 1
     if(uptime < 1000):
         uptime_info = "Uptime %d min." % uptime
     else:
         uptime_info = "Uptime %.1f h" % (uptime / 60.)
     lib.LCDdrawstring(0, 12, uptime_info.encode())
-    
+
     # line 2
     cpu_info = 'CPU %d%%' % loads
     lib.LCDdrawstring(0, 21, cpu_info.encode())
-    
+
     # line 3
-    used_mem = mem_info.used
-    ram_info = 'RAM {0} MB {1}%'.format(int(math.ceil(used_mem / 1024 / 1024)), int(mem_info.percent))
+    used_mem = mem_info['used']
+    ram_info = 'RAM {0} {1}%'.format(human_readable_size(used_mem), int(mem_info['percent']))
     lib.LCDdrawstring(0, 30, ram_info.encode())
-    
+
     # line 4
     show_round = int(datetime.now().timestamp() / 15 % 2)
     if show_round == 1:
@@ -66,27 +77,40 @@ def display_on_screen(uptime, loads, mem_info):
     else:
         net_info = psutil.net_if_addrs()
         ip_address = net_info['wlan0'][0].address
-        lib.LCDdrawstring(0, 39, ip_address.encode()) 
-    
+        lib.LCDdrawstring(0, 39, ip_address.encode())
+
     lib.LCDdisplay()
-    
+
 
 def show_cpu_info():
     while True:
-        uptime = (datetime.now().timestamp() - psutil.boot_time()) / 60
-        loads = psutil.cpu_percent(interval=1)
-        mem_info = psutil.virtual_memory()
-        display_on_screen(uptime, loads, mem_info)
-        lib.delay(2000)
+        if display_mode == 'pi':
+            uptime = (datetime.now().timestamp() - psutil.boot_time()) / 60
+            loads = psutil.cpu_percent(interval=1)
+            raw_mem_info = psutil.virtual_memory()
+            mem_info = {'used': raw_mem_info.used, 'percent': raw_mem_info.percent}
+            display_on_screen(uptime, loads, mem_info)
+            lib.delay(2000)
 
 
 def consume_method(ch, method, properties, body):
-    ins = json.loads(body.decode())
-    if ins['action'] == 1:
-        if ins['params']['led'] == 1:
+    global display_mode
+    msg = json.loads(body.decode())
+    if msg['action'] == 1:
+        if msg['params']['led'] == 1:
             GPIO.output(LED, ON)
         else:
             GPIO.output(LED, OFF)
+    elif msg['action'] == 2:
+        if display_mode != 'pi':
+            uptime = msg['params']['uptime'] / 1000 / 60
+            loads = msg['params']['loads']
+            mem_info = {'used': msg['params']['memUsed'], 'percent': msg['params']['memPercent']}
+            host_name = msg['params']['hostname']
+            display_on_screen(uptime, loads, mem_info, host_name)
+    elif msg['action'] == 3:
+        display_mode = msg['params']
+        pass
 
 
 def start_listen():
@@ -120,7 +144,8 @@ lib.delay(1000)
 
 flash_screen(3)
 
-if datetime.now().time() > dateutil.parser.parse('18:30').time() or datetime.now().time() < dateutil.parser.parse('7:00').time():
+now_time = datetime.now().time()
+if now_time > dateutil.parser.parse('18:30').time() or now_time < dateutil.parser.parse('7:00').time():
     GPIO.output(LED, ON)
 else:
     GPIO.output(LED, OFF)
